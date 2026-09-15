b85cdc973a6ea99b413dc5051e9099ebfb3e89de
Shiirevninj <shiirev0531@gmail.com>
2026-09-15 15:00:01 +0800

    threshold pass test hiiv

---
+++ Lab2/thresholds.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 5,
+  duration: '30s',
+
+  thresholds: {
+    http_req_duration: ['p(95)<2000'],
+    http_req_failed: ['rate<0.01'],
+  },
+};
+
+export default function () {
+  const res = http.get('https://test.k6.io');
+
+  check(res, {
+    'status 200 байна': (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
