5705f5604815e4ae3868a26d69921d0464d0af99
Shiirevninj <shiirev0531@gmail.com>
2026-09-22 18:52:10 +0800

    Chaos test bolon availability ur dun nemsen

--- Lab3/slo-test.js
+++ Lab3/slo-test.js
@@ -3,7 +3,7 @@ import { check, sleep } from 'k6';
 
 export const options = {
     vus: 20,
-    duration: '1m',
+    duration: '2m',
 
     thresholds: {
         'http_req_duration{name:cart}': ['p(95)<200'],
